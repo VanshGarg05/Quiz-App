485274c989e5cd7e3b6e12293e1c011be3a7769a
Vansh <vanshgarg05@gmail.com>
2024-02-09 23:32:05 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Quiz from './assets/Components/Quiz/Quiz'
+
+const App = () => {
+  return (
+    <React.Fragment>
+      <Quiz />
+    </React.Fragment>
+  )
+}
+
+export default App
